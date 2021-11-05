8bac4ab7f7847ada516fee74c4c654ab847b8b1e
Manage0 <61554465+Manage0@users.noreply.github.com>
2021-11-05 17:24:15 +0100

    Form not working yet

--- temalabor_drkeseruklaudia/src/App.js
+++ temalabor_drkeseruklaudia/src/App.js
@@ -5,20 +5,47 @@ import CVSection from './Pages/CVSection/CVSection';
 import ServicesSection from './Pages/ServicesSection/ServicesSection';
 import HIW from './Pages/HIWSection/HIW';
 import ContactSection from './Pages/ContactSection/ContactSection';
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { LangContext, LangProvider } from './Contexts/LangContext';
 import Price from './Pages/Price/PriceSection';
 import ImpSec from './Pages/ImpSection/ImpSec';
-import {BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
 
 function App() {
 
   //SEO
 
-  const[render, SetRender]=useState("true")
+  const [render, SetRender] = useState("true")
 
-  function Render(){
+  function Render() {
     SetRender(!render)
+  }
+
+  const Form = () => {
+    return (
+      <form name="contact" method="POST" data-netlify="true">
+        <input type="hidden" name="form-name" value="contact"></input>
+        <p>
+          <label>Your Name: <input type="text" name="name" /></label>
+        </p>
+        <p>
+          <label>Your Email: <input type="email" name="email" /></label>
+        </p>
+        <p>
+          <label>Your Role: <select name="role[]" multiple>
+            <option value="leader">Leader</option>
+            <option value="follower">Follower</option>
+          </select></label>
+        </p>
+        <p>
+          <label>Message: <textarea name="message"></textarea></label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
+
+    )
   }
 
   return (
@@ -27,19 +54,20 @@ function App() {
         <Router>
           <Switch>
             <Route exact path="/">
-            <Header/>
-              <IntroSection/>
-              <CVSection/>
-              <ServicesSection/>
-              <HIW/>
-              <Price/>
-              <ContactSection/> 
-              <ImpSec/>
+              <Form />
+              <Header />
+              <IntroSection />
+              <CVSection />
+              <ServicesSection />
+              <HIW />
+              <Price />
+              <ContactSection />
+              <ImpSec />
             </Route>
-            <Redirect to="/"/>
+            <Redirect to="/" />
           </Switch>
         </Router>
-      </LangProvider> 
+      </LangProvider>
     </div>
 
   );
--- temalabor_drkeseruklaudia/src/Pages/ContactSection/ContactSection.js
+++ temalabor_drkeseruklaudia/src/Pages/ContactSection/ContactSection.js
@@ -78,6 +78,32 @@ const ContactSection = () => {
 
     return (
         <div id="Contact">
+            <form name="contact" method="post" data-netlify="true" onSubmit="submit" netlify>
+                            <input type="hidden" name="form-name" value="contact" />
+                            <div id="FormTitle">
+                                {lang === "hun" ? "Ügyvédre van szüksége?" : "Brauchen Sie einen Anwalt?"}
+                            </div>
+                            <div>
+                                <label>Name
+                                <input type="text" class="input" name="name" placeholder={lang === "hun" ? "Adja meg a teljes nevét!" : "Bitte geben Sie Ihren vollen Namen ein!"} onChange={(e) => NameSet(e.target.value)} />
+                                </label>
+                            </div>
+                            <div>
+                                <label>Phone number</label>
+                                <input type="text" class="input" name="phone" placeholder={lang === "hun" ? "Adja meg a telefonszámát!" : "Bitte geben Sie Ihre Telefonnummer ein!"} onChange={(e) => PhoneSet(e.target.value)} />
+                            </div>
+                            <div>
+                                <label>E-mail</label>
+                                <input type="email" class="input" name="email" placeholder={lang === "hun" ? "Adja meg az e-mail címét!" : "Bitte geben Sie Ihre E-mail Adresse ein!"} onChange={(e) => EmailSet(e.target.value)} />
+                            </div>
+                            <div>
+                                <label>Case</label>
+                                <textarea class="input" id="description" name="case" placeholder={lang === "hun" ? "Írja le röviden kérdését! (max. 200 karakter)" : "Bitte beschreiben Sie Ihre Frage  kurz! (max. 200 Charakter)"} maxLength="200" rows="8" onChange={(e) => CaseSet(e.target.value)} />
+                            </div>
+                            <div id="btn-pos">
+                                <button id="button" type="submit" >{sendTitle}</button>
+                            </div>
+                        </form>
             <table id="ContactTable">
                 <tr>
                     <td id="ContactTD">
